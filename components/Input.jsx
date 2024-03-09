f456200b906386404296f30888c07601e6e16c7b
abdallah-abde <abdallah.abde@gmail.com>
2024-03-09 22:46:47 +0300

    add the input section of the feed component

---
+++ components/Input.jsx
@@ -0,0 +1,34 @@
+import { FaceSmileIcon, PhotoIcon } from "@heroicons/react/24/outline";
+
+export default function Input() {
+  return (
+    <div className="flex border-b border-gray-200 p-3 space-x-3">
+      <img
+        src="https://www.pinclipart.com/picdir/big/379-3796154_profile-clipart-john-doe-circle-png-download.png"
+        alt="user image"
+        className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
+      />
+      <div className="w-full divide-y divide-gray-200">
+        <div className="">
+          <textarea
+            rows="2"
+            placeholder="What's happening?"
+            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
+          />
+        </div>
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex">
+            <PhotoIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+            <FaceSmileIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+          </div>
+          <button
+            className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+            disabled
+          >
+            Tweet
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
