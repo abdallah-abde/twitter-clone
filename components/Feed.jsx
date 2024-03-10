55c41d04f3ca536266f58c9dfa74762b26d60491
abdallah-abde <abdallah.abde@gmail.com>
2024-03-10 22:10:57 +0300

    add the post section of the feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,31 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Abdallah Abde",
+      username: "abdallah-abde",
+      userImg:
+        "https://www.pinclipart.com/picdir/big/379-3796154_profile-clipart-john-doe-circle-png-download.png",
+      img: "https://images.unsplash.com/photo-1707343844152-6d33a0bb32c3?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Nice view",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Abdallah Abde",
+      username: "abdallah-abde",
+      userImg:
+        "https://www.pinclipart.com/picdir/big/379-3796154_profile-clipart-john-doe-circle-png-download.png",
+      img: "https://images.unsplash.com/photo-1707343843344-011332037abb?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Wow!",
+      timestamp: "2 days ago",
+    },
+  ];
+
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex items-center py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +35,9 @@ export default function Feed() {
         </div>
       </div>
       <Input />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 }
