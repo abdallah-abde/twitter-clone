a57202a00f1f95c240784ab5d52189e0b3526d7d
abdallah-abde <abdallah.abde@gmail.com>
2024-03-12 23:51:31 +0300

    get the session and modify sidebar and input components

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -10,8 +10,12 @@ import {
   InboxIcon,
   UserIcon,
 } from "@heroicons/react/24/outline";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
+  console.log(session);
+
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       <div className="hoverEffect p-2 hover:bg-blue-100  flex items-center justify-center">
@@ -24,30 +28,45 @@ export default function Sidebar() {
       <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
-        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-        <SidebarMenuItem text="Profile" Icon={UserIcon} />
-        <SidebarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
+        {session && (
+          <>
+            <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+            <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+            <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+            <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+            <SidebarMenuItem text="Profile" Icon={UserIcon} />
+            <SidebarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
+          </>
+        )}
       </div>
 
-      <button className="bg-blue-400 text-white rounded-full  w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-        Tweet
-      </button>
-
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img
-          src="https://www.pinclipart.com/picdir/big/379-3796154_profile-clipart-john-doe-circle-png-download.png"
-          alt="user image"
-          className="w-10 h-10 rounded-full xl:mr-2"
-        />
-        <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">Abdallah Abde</h4>
-          <p className="text-gray-500">@abdallah-abde</p>
-        </div>
-        <EllipsisHorizontalCircleIcon className="h-5 xl:ml-8 hidden xl:inline" />
-      </div>
+      {session ? (
+        <>
+          <button className="bg-blue-400 text-white rounded-full  w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+            <img
+              src={session.user.image}
+              alt="user image"
+              className="w-10 h-10 rounded-full xl:mr-2"
+              onClick={signOut}
+            />
+            <div className="leading-5 hidden xl:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <EllipsisHorizontalCircleIcon className="h-5 xl:ml-8 hidden xl:inline" />
+          </div>
+        </>
+      ) : (
+        <button
+          className="bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+          onClick={signIn}
+        >
+          Sign in
+        </button>
+      )}
     </div>
   );
 }
