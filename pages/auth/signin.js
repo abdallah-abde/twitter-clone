6b67ac6da52ee04ebaba1e773c4fc84f7c767bdc
abdallah-abde <abdallah.abde@gmail.com>
2024-03-17 22:13:35 +0300

    fix deployment errors

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -10,7 +10,7 @@ export default function signin({ providers }) {
       />
       <div className="">
         {Object.values(providers).map((provider) => (
-          <div className="flex flex-col items-center">
+          <div key={provider.name} className="flex flex-col items-center">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/220px-Logo_of_Twitter.svg.png"
               alt="twitter logo"
